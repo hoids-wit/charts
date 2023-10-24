7c58adad12638d7c11e1cb3f73f7d077bb64f938
Niklis Petecca <niklis.petecca@gmail.com>
2023-10-24 08:12:48 -0400

    first matrix attempt

--- matrix/setup.bash
+++ matrix/setup.bash
@@ -2,7 +2,12 @@ sudo mount -t nfs 192.168.86.196:/mnt/user/matrix /mnt/matrix/data
 sudo docker run -it --rm -v /mnt/matrix/data:/data -e SYNAPSE_SERVER_NAME=matrix.petecca.com -e SYNAPSE_REPORT_STATS=no matrixdotorg/synapse:latest generate
 
 sudo microk8s helm repo add ananace-charts https://ananace.gitlab.io/charts
-sudo microk8s helm install matrix-synapse ananace-charts/matrix-synapse --set serverName=matrix.petecca.com --set wellknown.enabled=true
+sudo microk8s helm install matrix-synapse . --set serverName=matrix.petecca.com --set wellknown.enabled=true
+
+rm /mnt/matrix/data/* -r -f
+sudo microk8s helm uninstall matrix-synapse -n default
+
+
 
 export POD_NAME=$(sudo microk8s kubectl get pods --namespace default -l "app.kubernetes.io/name=matrix-synapse,app.kubernetes.io/instance=matrix-synapse,app.kubernetes.io/component=synapse" -o jsonpath="{.items[0].metadata.name}")
     sudo microk8s kubectl exec --namespace default $POD_NAME -- register_new_matrix_user -c /synapse/config/homeserver.yaml -c /synapse/config/conf.d/secrets.yaml -u matrixaadmin -p gravelporkballsnailskin --admin http://localhost:8008 
