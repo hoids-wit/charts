dfbe55982efb96daf050170600def5cb7abd7d00
Niklis Petecca <niklis.petecca@gmail.com>
2024-03-15 16:52:49 -0400

    added non-working matrix updated common deployment first attempt at tooljet

---
+++ llmstack/script.bash
@@ -0,0 +1,11 @@
+sudo apt-get update 
+sudo apt install docker.io -y
+sudo apt install python3-pip -y
+sudo apt install python-is-python3 -y
+sudo apt install libgl1-mesa-glx -y
+sudo pip install playwright
+sudo playwright install-deps
+sudo playwright install
+
+sudo pip install llmstack
+sudo llmstack
